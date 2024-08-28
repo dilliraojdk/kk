2192bfffb1fa33fa8b17d986fdda32c57f70806c
TL <TL@gmail.com>
2024-08-28 22:09:31 +0530

    to modify in sb main class

---
+++ DemoExamplesApplication.java
@@ -0,0 +1,14 @@
+package com.app.pojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoExamplesApplication.class, args);
+		  System.out.println("Basic Programs in SB Application");
+	}
+
+}
